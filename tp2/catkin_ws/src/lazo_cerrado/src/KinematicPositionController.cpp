1d749cc125424cd31ef9f750c8000b3f7f27d93d
Hernan Bari <hernansoyrecapo@hotmail.com>
2016-09-23 19:11:53 -0300

    Nuevos logs ej 1 y códgio ej3

--- tp2/catkin_ws/src/lazo_cerrado/src/KinematicPositionController.cpp
+++ tp2/catkin_ws/src/lazo_cerrado/src/KinematicPositionController.cpp
@@ -1,6 +1,7 @@
 #include <angles/angles.h>
 #include "KinematicPositionController.h"
 #include "tf_utils.hpp"
+#include "vector"
 #include "math.h"
 
 KinematicPositionController::KinematicPositionController(ros::NodeHandle& nh) :
@@ -51,9 +52,9 @@ bool KinematicPositionController::getCurrentPose(const ros::Time& t, double& x, 
  * - K_RHO < K_ALPHA
  * - K_BETA < 0
  */
-#define K_RHO 9
-#define K_ALPHA 10
-#define K_BETA -10
+#define K_RHO .15
+#define K_ALPHA .15 
+#define K_BETA -.30
 
 bool KinematicPositionController::control(const ros::Time& t, double& v, double& w)
 {
@@ -92,7 +93,7 @@ bool KinematicPositionController::control(const ros::Time& t, double& v, double&
   v = K_RHO*rho;
   w = K_ALPHA*alpha+K_BETA*beta;
 
-  ROS_INFO_STREAM("atan2: " << atan2(dy, dx) << " theta siegwart: " << theta << " expected_atheta: "  << current_a << " rho: " << rho << " alpha: " << alpha << " beta: " << beta << " v: " << v << " w: " << w);
+  //ROS_INFO_STREAM("atan2: " << atan2(dy, dx) << " theta siegwart: " << theta << " expected_atheta: "  << current_a << " rho: " << rho << " alpha: " << alpha << " beta: " << beta << " v: " << v << " w: " << w);
 
   return true;
 }
@@ -103,7 +104,7 @@ double dist2(double x0, double y0, double x1, double y1)
 
 // Lookahead distance
 #define LKAHD_DIST 1
-set<int> prev_wpoints;
+std::vector<unsigned int> prev_wpoints;
 
 bool KinematicPositionController::getPursuitBasedGoal(const ros::Time& t, double& x, double& y, double& a)
 {
@@ -124,8 +125,12 @@ bool KinematicPositionController::getPursuitBasedGoal(const ros::Time& t, double
   if (trajectory.points.size() == 0){
     return false;
   }
-  int nearest_wpoint = -1;
-  double nearest_dist = -1;
+  unsigned int nearest_wpoint = 0;
+  double nearest_wpoint_x = -1;
+  double nearest_wpoint_y = -1;
+  double nearest_wpoint_a = -1;
+  double nearest_dist = INT_MAX;
+  
   for(unsigned int i = 0; i < trajectory.points.size(); i++)
   {
     // Recorren cada waypoint definido
@@ -137,8 +142,9 @@ bool KinematicPositionController::getPursuitBasedGoal(const ros::Time& t, double
     double wpoint_a = tf2::getYaw(wpoint.transform.rotation);
     
     //...
+  
     double dist_to_wpoint = dist2(current_x, current_y, wpoint_x, wpoint_y);
-    if (nearest_wpoint == -1 || (find(prev_wpoints.begin(), prev_wpoints.end(),i) !=  prev_wpoints.end() && dist_to_wpoint < nearest_dist){
+    if (find(prev_wpoints.begin(), prev_wpoints.end(),i) ==  prev_wpoints.end() && dist_to_wpoint < nearest_dist){
       nearest_wpoint = i;
       nearest_wpoint_x = wpoint_x;
       nearest_wpoint_y = wpoint_y;
@@ -146,60 +152,66 @@ bool KinematicPositionController::getPursuitBasedGoal(const ros::Time& t, double
       nearest_dist = dist_to_wpoint;
     }
   }
-  if (nearest_dist > LKAHD_DIST || i == trajectory.points.size()-1){
+
+  if (nearest_dist > LKAHD_DIST || nearest_wpoint == trajectory.points.size()-1){
     x = nearest_wpoint_x;
     y = nearest_wpoint_y;
     a = nearest_wpoint_a;
   }else {
     
-    prev_wpoints.insert(nearest_wpoint);
+    prev_wpoints.push_back(nearest_wpoint);
 
-    const robmovil_msgs::TrajectoryPoint& near_wpoint = trajectory.points[nearest_wpoint];
-    double x0 = near_wpoint.transform.translation.x;
-    double y0 = near_wpoint.transform.translation.y;
+    // const robmovil_msgs::TrajectoryPoint& near_wpoint = trajectory.points[nearest_wpoint];
+    // double x0 = near_wpoint.transform.translation.x;
+    // double y0 = near_wpoint.transform.translation.y;
 
     const robmovil_msgs::TrajectoryPoint& next_wpoint = trajectory.points[nearest_wpoint+1];
     double x1 = next_wpoint.transform.translation.x;
     double y1 = next_wpoint.transform.translation.y;
+    double a1 = tf2::getYaw(next_wpoint.transform.rotation);
     
-    // Recta entre dos puntos:
-   // m = (y1-y0)/(x1-x0)
-   // c = m*(-x0)+y0
+   //  // Recta entre dos puntos:
+   // // m = (y1-y0)/(x1-x0)
+   // // c = m*(-x0)+y0
 
-   // Interseccion circulo y recta:
-   // y = mx+c
-   // r^2 = (x-p)^2+(y-q)^2
+   // // Interseccion circulo y recta:
+   // // y = mx+c
+   // // r^2 = (x-p)^2+(y-q)^2
    
-   // A = m^2+1
-   // B = 2(mc-mq-p)
-   // C = q^2-r^2+p^2-2cq+c^2
+   // // A = m^2+1
+   // // B = 2(mc-mq-p)
+   // // C = q^2-r^2+p^2-2cq+c^2
 
-   // x = (-B+-sqrt(B^2-4*A*C))/2*A
-   // y = m*x+c
+   // // x = (-B+-sqrt(B^2-4*A*C))/2*A
+   // // y = m*x+c
   
-    // Recta entre dos puntos:  
-    double m = (y1-y0)/(x1-x0);
-    double c = m*(-x0)+y0;
-    double p = current_x;
-    double q = current_y;
+   //  // Recta entre dos puntos:  
+   //  double m = (y1-y0)/(x1-x0);
+   //  double c = m*(-x0)+y0;
+   //  double p = current_x;
+   //  double q = current_y;
 
 
-    // Interseccion circulo y recta:
-    double A = m^2+1;
-    double B = 2*(m*c-m*q-p);
-    double C = q^2-r^2+p^2-2*c*q+c^2;
+   //  // Interseccion circulo y recta:
+   //  double A = pow(m,2)+1;
+   //  double B = 2*(m*c-m*q-p);
+   //  double C = pow(q,2)-pow(LKAHD_DIST,2)+pow(p,2)-2*c*q+pow(c,2);
 
 
 
-    x = (-B+-sqrt(B^2-4*A*C))/2*A;
-    y = m*x+c;
+   //  x = (-B+sqrt(pow(B,2)-4*A*C))/2*A;
+   //  y = m*x+c;
 
-    // La orientacion a seguir es la pendiente de la recta (positiva o negativa???)
-    a = m;
+   //  // La orientacion a seguir es la pendiente de la recta (positiva o negativa???)
+   //  a = m;
+
+    x = x1;
+    y = y1;
+    a = a1;
    
 
   }
-
+  // ROS_INFO_STREAM("wpoints: " << trajectory);
 
   
   /* retorna true si es posible definir un goal, false si se termino la trayectoria y no quedan goals. */
